c7140439d3733b771c3c4086a365ce1fe0597524
5V715 <github@silas.dev>
2026-07-04 21:40:15 +0200

    feat: add BLE service, characteristics, and advertising

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -2,11 +2,28 @@
 #include <NimBLEDevice.h>
 #include "ble_protocol.h"
 
+static NimBLECharacteristic* outboxCharacteristic = nullptr;
+
 void setup() {
   Serial.begin(115200);
   delay(1000);
   Serial.println("BLE local chat hub booting");
+
   NimBLEDevice::init("BLE Local Chat Hub");
+
+  NimBLEServer* server = NimBLEDevice::createServer();
+  NimBLEService* service = server->createService(HUB_SERVICE_UUID);
+
+  service->createCharacteristic(INBOX_CHARACTERISTIC_UUID, NIMBLE_PROPERTY::WRITE);
+  outboxCharacteristic = service->createCharacteristic(OUTBOX_CHARACTERISTIC_UUID, NIMBLE_PROPERTY::NOTIFY);
+
+  service->start();
+
+  NimBLEAdvertising* advertising = NimBLEDevice::getAdvertising();
+  advertising->addServiceUUID(HUB_SERVICE_UUID);
+  advertising->start();
+
+  Serial.println("Advertising started");
 }
 
 void loop() {
